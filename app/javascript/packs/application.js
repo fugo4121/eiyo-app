9c171c2422b2aa44f6e15abd0736ab63d70cfa20
fugo4121 <lancerevo345@yahoo.co.jp>
2020-11-11 01:51:53 +0900

    bootstrap、awesome導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,5 @@ require("sleep_kcal")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//= require jquery
+//= require bootstrap-sprockets
