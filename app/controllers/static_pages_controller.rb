3fe0e49003d750f716422144f4a18439d99a2d45
fugo4121 <lancerevo345@yahoo.co.jp>
2020-12-06 15:08:23 +0900

    グラフ追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,19 @@
 class StaticPagesController < ApplicationController
   def home
-  end
+    # 今日の日付
+    @standard_date = Time.current
 
-  def top
-  end
+    @user = User.find(current_user.id)
 
-  def staff
+    # 体重グラフ表示用
+    @healths = Health.where(user_id: current_user.id)
+    # 体重データを配列で取得
+    @weights = @healths.map(&:weight)
+    @dates = @healths.map{|health| health.start_time.strftime('%Y/%m/%d') }
+
+    @meals = Meal.where(user_id: current_user.id, start_time: @standard_date).includes(:food)
+    # binding.pry
+    @bases = Base.all
+
   end
 end
--- app/models/base.rb
+++ app/models/base.rb
@@ -1,12 +1,16 @@
 class Base < ActiveHash::Base
   self.data = [
     # 女性・年齢バージョンも追加
+
+    # 最小値
     { id: 1,  kcal: 2488, protein: 87.4,
               lipid: 59.7, carb: 336, calcium: 750, iron: 7.5, vitamin_a: 900,
               vitamin_b1: 1.4, vitamin_b2: 1.6, vitamin_c: 100, vitamin_e: 6,
-              fiber: 21, salt: 0},
+              fiber: 21, salt: 5},
+    
+    # 最大値(9999は上限なし)
     { id: 2,  kcal: 2888, protein: 134.4,
-      lipid: 89.6, carb: 436.8, calcium: 2500, iron: 50, vitamin_a: 2700,
-      vitamin_b1: 999, vitamin_b2: 999, vitamin_c: 9999, vitamin_e: 900,
-      fiber: 999, salt: 7.5}]
+              lipid: 89.6, carb: 436.8, calcium: 2500, iron: 50, vitamin_a: 2700,
+              vitamin_b1: 9999, vitamin_b2: 9999, vitamin_c: 9999, vitamin_e: 900,
+              fiber: 9999, salt: 7.5}]
 end
